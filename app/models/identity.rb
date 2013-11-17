19d762ed1c9c2857baea726be7e4db16d9651291
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-17 11:20:57 -0800

    Fix up `Identity` implementation to work properly with identity and gh auth

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,10 @@
 class Identity < ModelBase
 
+  include ::OmniAuth::Identity::Model
+  include ::OmniAuth::Identity::SecurePassword
+
+  has_secure_password
+
   def before_create
     values[:provider_identity] = [values[:provider], values[:uid]].compact.join('_')
   end
@@ -12,14 +17,16 @@ class Identity < ModelBase
 
   bucket :identities
 
-  value :provider_identity
+  value :provider_identity, :class => String
   value :uid, :class => String
   value :provider, :class => String
   value :email, :class => String
-  value :password, :class => String
   value :credentials
   value :extras
-  value :info, :class => Hash, :default => {}
+  value :data
+  value :password_digest, :class => String
+
+  attr_accessor :password_confirmation
 
   link :user, User
 
@@ -27,16 +34,18 @@ class Identity < ModelBase
 
   class << self
 
-    def auth_key
-      'uid'
-    end
-
     def lookup_key(uid, provider=nil)
       key = [provider, uid].compact.join('_').to_sym
     end
 
     def lookup(uid, provider=nil)
       self[lookup_key(uid, provider)]
+    end
+
+    def locate(search_hash)
+      puts '&' * 50
+      p search_hash
+      raise 'wat'
     end
 
     def find_or_create_via_omniauth(attributes, existing_user=nil)
@@ -48,19 +57,55 @@ class Identity < ModelBase
       else
         Rails.logger.info "No existing identity found! Creating new user"
         username = attributes[:info].try(:[], :nickname) ||
+          attributes[:info].try(:[], :login) ||
           attributes[:info].try(:[], :email) ||
           unique_id
-        user = User.new(username).save
-        identity = Identity.new(lookup_key(unique_id, provider))
-        identity[:data] = attributes
-        identity[:provider] = provider
-        identity[:uid] = unique_id
-        identity.user = user
-        identity.save
+        user = User.by_username(username)
+        if(user)
+        else
+          identity = Identity.create(
+            attributes.merge(
+              :username => username,
+              :unique_id => unique_id,
+              :password => 'stub'
+            )
+          )
+          identity.data = attributes
+          identity.provider = provider
+          identity.uid = unique_id
+          identity.password = 'stub'
+          unless(identity.save)
+            Rails.logger.error identity.errors.inspect
+            raise identity.errors unless identity.save
+          end
+        end
       end
       identity
     end
-    alias_method :create, :find_or_create_via_omniauth
+
+    def create(attributes)
+      username = attributes[:username] || attributes[:unique_id]
+      user = User.by_username(username)
+      raise 'user exists' if user
+      user = User.new
+      user.username = username
+      user.save
+      user.create_account
+
+      Rails.logger.info "New user creation: #{attributes.inspect}"
+
+      identity = Identity.new
+      identity.provider_identity = lookup_key(attributes[:unique_id], 'fission')
+      identity.password = attributes[:password]
+      identity.email = attributes[:email]
+      identity.uid = attributes[:unique_id]
+      identity.user = user
+      unless(identity.save)
+        Rails.logger.error identity.errors.inspect
+        raise identity.errors
+      end
+      identity
+    end
 
   end
 
