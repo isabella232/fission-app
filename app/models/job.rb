299aee89e7f75da7935dee429ec948330e61c2b6
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-17 16:39:10 -0800

    Include type casted values and set default display attributes

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,17 @@
 class Job < ModelBase
   bucket :jobs
 
+  value :task, :class => String
+  value :status, :class => String
+  value :last_update, :class => Time
+  value :percent_complete, :class => Fixnum
+
   link :user, User
   link :account, Account
+
+  class << self
+    def display_attributes
+      [:key, :task, :status, :percent_complete, :last_update]
+    end
+  end
 end
