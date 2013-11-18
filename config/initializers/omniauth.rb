cbd4393c2dff6dbeb7c5990f03379e446b508ee1
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-17 16:06:14 -0800

    Cleanup omniauth provider setup

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if(Rails.env.production? || ENV['OMNIAUTH_ENABLE'])
+  if(Rails.env.production? || ENV['ENABLE_OMNIAUTH'])
     Rails.application.config.omniauth_provider = :identity
   else
     Rails.application.config.omniauth_provider = :developer
   end
 
-  provider :identity, :fields => [:unique_id], :uid_field => :unique_id, :on_failed_registration => UsersController.action(:new)
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider(:identity,
+    :fields => [:username],
+    :uid_field => :unique_id,
+    :on_failed_registration => UsersController.action(:new)
+  )
+  provider(:github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'])
 end
 
 OmniAuth.config.on_failure = Proc.new { |env|
