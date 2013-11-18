7234caf0b3ca6fbaf8ac0f009bf58c34c6b6e5c9
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-17 16:02:13 -0800

    Add restrictor to account and friendly string output

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,6 +13,14 @@ class Account < ModelBase
     def display_attributes
       [:name, :owner]
     end
+
+    def restrict(user)
+      ([user.base_account] + user.accounts).compact.uniq
+    end
+  end
+
+  def to_s
+    name
   end
 
 end
