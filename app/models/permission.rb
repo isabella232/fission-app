377e644f81fde2c311856f7b8ec897fd42f2b931
Chris Roberts <chrisroberts.code@gmail.com>
2013-11-25 22:35:47 -0800

    Remove deprecated model

--- app/models/permission.rb
+++
@@ -1,9 +0,0 @@
-# attrs:
-#   * name
-#   * description
-class Permission < ActiveRecord::Base
-  validates :name, :presence => true, :uniqueness => true
-
-  has_many :users, :through => :user_permissions
-  has_many :accounts, :through => :account_permissions
-end
