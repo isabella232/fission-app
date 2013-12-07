91c1da28707e7d2e435fce7af17b86154133dfb4
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-07 10:40:50 -0800

    Add rest endpoint values to configuration

--- config/initializers/fission.rb
+++ config/initializers/fission.rb
@@ -4,5 +4,7 @@ class FissionApp::Application
 
   config.fission.json_support = :jsend
   config.fission.site_brand = 'Fission'
+  config.fission.rest_endpoint = 'http://alpha.hw-ops.com'
+  config.fission.rest_endpoint_ssl = 'https://alpha.hw-ops.com'
 
 end
