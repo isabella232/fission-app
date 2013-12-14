c9ab547712340651fd329272a720ea3ad6e92d6d
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-14 08:09:58 -0800

    Lets use a JSON file to configure the app

--- config/initializers/fission.rb
+++ config/initializers/fission.rb
@@ -2,9 +2,17 @@ class FissionApp::Application
 
   config.fission = ActiveSupport::Configurable::Configuration.new
 
-  config.fission.json_support = :jsend
-  config.fission.site_brand = 'Fission'
-  config.fission.rest_endpoint = 'http://alpha.hw-ops.com'
-  config.fission.rest_endpoint_ssl = 'https://alpha.hw-ops.com'
+  config.fission.config = JSON.load(
+    File.read(
+      File.join(File.dirname(__FILE__), '..', 'fission.json')
+    )
+  ).with_indifferent_access
+
+  config.fission.json_support = config.fission.config[:json_style]
+  config.fission.site_brand = config.fission.config[:site_brand]
+  config.fission.rest_endpoint = config.fission.config[:fission][:rest_endpoint]
+  config.fission.rest_endpoint_ssl = config.fission.config[:fission][:rest_endpoint_ssl]
+
+  config.fission.static_pages = config.fission.config[:static_pages][:path]
 
 end
