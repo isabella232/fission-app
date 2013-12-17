36eaedcd43369b0af74a73324c1795c884f5bbf2
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-16 19:48:22 -0800

    Update Verion class implementation since jruby is being cranky about it

--- lib/fission-app/version.rb
+++ lib/fission-app/version.rb
@@ -1,9 +1,15 @@
 module FissionApp
-  class Version < Gem::Version
-    attr_reader :codename
-    def initialize(version, codename=nil)
-      super(version)
+  class Version
+    attr_accessor :codename
+    attr_reader :version
+
+    def initialize(version, codename)
       @codename = codename
+      @version = Gem::Version.new(version)
+    end
+
+    def method_missing(*args)
+      version.send(*args)
     end
   end
   VERSION = Version.new('0.1.0', 'Shut up and take my money')
