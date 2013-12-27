3022e8246e639d7238812ea1ea85ac57760517ed
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-27 13:16:35 -0800

    Remove rake conditional (found loaded during app runtime)

--- config/initializers/backgroundable_jobs.rb
+++ config/initializers/backgroundable_jobs.rb
@@ -1,5 +1,3 @@
-unless(defined?(Rake))
-  # TODO: Allow specifying carnivore source for sending payloads into fission
-  require 'fission-app/backgroundable'
-  Rails.application.config.backgroundable = Fission::App::Backgroundable.new
-end
+# TODO: Allow specifying carnivore source for sending payloads into fission
+require 'fission-app/backgroundable'
+Rails.application.config.backgroundable = Fission::App::Backgroundable.new
