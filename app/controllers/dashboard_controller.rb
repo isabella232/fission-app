56fb29506a6e472192303c9a08cf9edbcc7d714a
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-30 13:19:13 -0800

    Set unregistered repos for view to use in form

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,16 @@ class DashboardController < ApplicationController
   def index
     @accounts = Account.restrict(current_user)
     @repos = Repository.restrict(current_user)
+    @unregistered_repos = unregistered_repos(@repos)
+  end
+
+  protected
+
+  def unregistered_repos(repos)
+    registered_names = repos.map(&:name)
+    fetch_github_repos(*current_user.managed_accounts.map(&:name)).find_all do |repo|
+      !registered_names.include?(repo.full_name)
+    end.sort{|x,y| x.full_name <=> y.full_name }
   end
 
 end
