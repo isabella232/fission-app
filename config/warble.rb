621d32441565181a273c616e6508c55ebdf898d5
Chris Roberts <chrisroberts.code@gmail.com>
2013-12-30 17:41:51 -0800

    Timestamp packages when they are built

--- config/warble.rb
+++ config/warble.rb
@@ -1,7 +1,7 @@
 Warbler::Config.new do |config|
   config.features = %w(executable)
   config.java_libs += FileList["lib/java/*.jar"]
-  config.jar_name = 'fission'
+  config.jar_name = "fission-#{Time.now.to_i}"
   config.override_gem_home = true
   config.webserver = 'jetty'
   config.webxml.jruby.rack.logging = 'slf4j'
