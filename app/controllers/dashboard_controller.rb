8c7e438d5bdcfc7438c40b00b3b99c9bc004d5f2
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-07 07:53:11 -0800

    Clean up how we cache the remote repo lookup

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,22 +3,23 @@ class DashboardController < ApplicationController
   def index
     @accounts = Account.restrict(current_user)
     @repos = Repository.restrict(current_user)
-    # TODO: This needs to be timed and managed internally
-    if(!current_user.session(:unregistered_repos))
-      current_user.set_session(:unregistered_repos, unregistered_repos(@repos))
-      current_user.save
-    end
-    @unregistered_repos = (current_user.session(:unregistered_repos) || []).map(&:with_indifferent_access)
+    @unregistered_repos = unregistered_repos(@repos)
   end
 
   protected
 
   def unregistered_repos(repos)
     registered_names = repos.map(&:name)
-    fetch_github_repos(*current_user.managed_accounts.map(&:name)).find_all do |repo|
-      !registered_names.include?(repo.full_name)
-    end.sort{|x,y| x.full_name <=> y.full_name }.map do |repo|
-      {:full_name => repo.full_name}.with_indifferent_access
+    unless(current_user.session(:repositories, :github))
+      current_user.set_session(
+        :repositories, :github,
+        fetch_github_repos(*current_user.managed_accounts.map(&:name)).map(&:full_name)
+      )
+    end
+    current_user.session(:repositories, :github).find_all do |repo_name|
+      !registered_names.include?(repo_name)
+    end.sort{|x,y| x <=> y }.map do |repo_name|
+      {:full_name => repo_name}.with_indifferent_access
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -65,8 +65,7 @@ class SessionsController < ApplicationController
   def destroy
     respond_to do |format|
       format.html do
-        current_user.session_data = {}.with_indifferent_access
-        current_user.save
+        current_user.clear_session!
         reset_session
         redirect_to root_url, notice: 'Logged out'
       end
