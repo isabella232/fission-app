9bde3dafcea6710870d141e8b45afb1c82c401bc
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-17 08:04:43 -0800

    Update access methodology to user session store

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,13 +10,13 @@ class DashboardController < ApplicationController
 
   def unregistered_repos(repos)
     registered_names = repos.map(&:name)
-    unless(current_user.session(:repositories, :github))
-      current_user.set_session(
+    unless(current_user.session.get(:repositories, :github))
+      current_user.session.set(
         :repositories, :github,
         fetch_github_repos(*current_user.managed_accounts.map(&:name)).map(&:full_name)
       )
     end
-    current_user.session(:repositories, :github).find_all do |repo_name|
+    current_user.session.get(:repositories, :github).find_all do |repo_name|
       !registered_names.include?(repo_name)
     end.sort{|x,y| x <=> y }.map do |repo_name|
       {:full_name => repo_name}.with_indifferent_access
