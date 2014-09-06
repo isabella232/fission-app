b11c5afeeec42376cb09fed675a715e5c6f2fbbd
Chris Roberts <chrisroberts.code@gmail.com>
2014-09-06 09:44:47 -0700

    Only define dashboard related routes by default

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 FissionApp::Application.routes.draw do
-
   get 'dashboard', :to => 'dashboard#index', :as => :dashboard
-
-  scope :utilities do
-    post 'form_mailer', :to => 'utilities#form_mailer', :as => :form_mailer_utility
-  end
-
-  root 'static_pages#show'
-
+  root 'dashboard#index'
 end
