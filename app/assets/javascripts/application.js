5a58c00648975bcf33a532772fbc3f947ab30d0e
Chris Roberts <chrisroberts.code@gmail.com>
2014-10-21 06:59:20 -0700

    Fix builtin bootstrap asset loading

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap
+//= require bootstrap-sprockets
 //= require moment
 //= require bootstrap-datetimepicker
 //= require window_rails
