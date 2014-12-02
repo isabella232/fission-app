2d17cbe15168b57ecf694386695e7a1468278d1a
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-01 19:08:29 -0800

    Remove dashboard and root entries

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 FissionApp::Application.routes.draw do
-  get 'dashboard', :to => 'dashboard#index', :as => :dashboard
   get 'error', :to => 'application#error', :as => :error
-  root 'dashboard#index'
 end
