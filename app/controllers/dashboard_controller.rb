93b89ef6cf7b8cc04089f73ad46f3a033c85ea67
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-02 07:39:00 -0800

    Add top level dashboard implementation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,29 @@
 class DashboardController < ApplicationController
 
-  def index
+  before_action :validate_user_permission!, :except => [:summary]
+
+  def summary
+    respond_to do |format|
+      format.js do
+        flash[:error] = 'Unsupported request!'
+        javascript_redirect_to repository_listing_endpoint
+      end
+      format.html do
+        @cells = {}.with_indifferent_access.tap do |cells|
+          current_user.active_products.each do |product|
+            Rails.application.railties.engines.each do |eng|
+              if(eng.respond_to?(:fission_product))
+                if(eng.fission_product.include?(product))
+                  if(eng.respond_to?(:fission_dashboard))
+                    cells.merge!(eng.fission_dashboard.with_indifferent_access)
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
   end
 
 end
