f0f6da48e9ac2c34a318aa588e506f48c1dd1919
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-02 14:22:06 -0800

    Add account switch route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 FissionApp::Application.routes.draw do
+  get 'switch/:account_id', :to => 'application#switch', :as => :switch
   get 'dashboard', :to => 'dashboard#summary', :as => :dashboard
   get 'error', :to => 'application#error', :as => :error
 end
