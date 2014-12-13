e72fb67dd05892da07261e746e36e3771ac1f873
Chris Roberts <chrisroberts.code@gmail.com>
2014-12-13 06:29:32 -0800

    When we have product set for request, stash in the current_user's run_state

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -110,6 +110,10 @@ class ApplicationController < ActionController::Base
         @product = Product.find_by_internal_name(path_parts[1])
       end
     end
+    if(@product)
+      current_user.run_state.current_product = @product
+    end
+    @product
   end
 
   # Check that user has permission to defined paths
