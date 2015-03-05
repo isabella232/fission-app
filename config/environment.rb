6ee0fff084a2735dddadffe578163bc4dca49bc6
Chris Roberts <chrisroberts.code@gmail.com>
2015-03-04 16:35:34 -0800

    :fu: rails. i do what i want!

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,20 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-# Initialize the Rails application.
 unless(ENV['RAILS_ASSETS_PRECOMPILE'])
+  FissionApp::Application.initialize!
+else
+  class FissionApp::Application
+    def run_initializers(group=:default, *args)
+      return if instance_variable_defined?(:@ran)
+      initializers.tsort_each do |initializer|
+        begin
+          initializer.run(*args) if initializer.belongs_to?(group)
+        rescue
+        end
+      end
+      @ran = true
+    end
+  end
   FissionApp::Application.initialize!
 end
