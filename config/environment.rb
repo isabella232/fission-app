2c5468bb451708b10b7a6bae91954df1dc3c3f01
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-23 09:48:58 -0700

    When precompiling assets, force rails to _not_ run prepare blocks \\\\o////

--- config/environment.rb
+++ config/environment.rb
@@ -1,20 +1,16 @@
 # Load the Rails application.
-require File.expand_path('../application', __FILE__)
-
-unless(ENV['RAILS_ASSETS_PRECOMPILE'])
-  FissionApp::Application.initialize!
-else
-  class FissionApp::Application
-    def run_initializers(group=:default, *args)
-      return if instance_variable_defined?(:@ran)
-      initializers.tsort_each do |initializer|
-        begin
-          initializer.run(*args) if initializer.belongs_to?(group)
-        rescue
+if(ENV['RAILS_ASSETS_PRECOMPILE'])
+  require 'rails/railtie/configuration'
+  module Rails
+    class Railtie
+      class Configuration
+        def to_prepare_blocks(*args, &block)
+          []
         end
       end
-      @ran = true
     end
   end
-  FissionApp::Application.initialize!
 end
+
+require File.expand_path('../application', __FILE__)
+FissionApp::Application.initialize!
