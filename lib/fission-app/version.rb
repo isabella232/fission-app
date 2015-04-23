df1c00e61320257a82803d7d71fea6e3411d7b3f
Chris Roberts <chrisroberts.code@gmail.com>
2015-04-23 14:55:15 -0700

    Now lets bump the version for development 0.1.7

--- lib/fission-app/version.rb
+++ lib/fission-app/version.rb
@@ -12,5 +12,5 @@ module FissionApp
       version.send(*args)
     end
   end
-  VERSION = Version.new('0.1.6', 'Posers! I was hating Zoidberg before it was cool.')
+  VERSION = Version.new('0.1.7', 'Posers! I was hating Zoidberg before it was cool.')
 end
