a42795d552f83bab5f7eef728b9dfaea71cb3d4c
Chris Roberts <code@chrisroberts.org>
2015-05-22 14:34:38 -0700

    Update version for development 0.1.11

--- lib/fission-app/version.rb
+++ lib/fission-app/version.rb
@@ -12,5 +12,5 @@ module FissionApp
       version.send(*args)
     end
   end
-  VERSION = Version.new('0.1.10', "I'm the first one at work. A new low.")
+  VERSION = Version.new('0.1.11', "I'm the first one at work. A new low.")
 end
