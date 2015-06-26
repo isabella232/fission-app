fb2f9e70be29f4010388fc6e02d794cfbb14c0b0
Chris Roberts <code@chrisroberts.org>
2015-06-26 16:20:32 -0700

    Disable compressions until syntax issues are resolved

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,8 @@ FissionApp::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.js_compressor = false
+  config.assets.css_compressor = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
