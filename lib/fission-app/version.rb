8d8ea30c977fa6c3ab7a35fd64f3bdce22295747
Chris Roberts <code@chrisroberts.org>
2015-06-25 19:37:20 -0700

    Update version for development 0.1.30

--- lib/fission-app/version.rb
+++ lib/fission-app/version.rb
@@ -12,5 +12,5 @@ module FissionApp
       version.send(*args)
     end
   end
-  VERSION = Version.new('0.1.30', 'Who are you, my warranty?')
+  VERSION = Version.new('0.1.31', 'Who are you, my warranty?')
 end
