91b783e0c480837d65648ca5504f51e16970f6fb
Chris Roberts <code@chrisroberts.org>
2015-07-02 07:36:09 -0700

    Add routing for status path

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ FissionApp::Application.routes.draw do
   get 'switch(/:account_id)', :to => 'application#switch', :as => :account_switch
   get 'dashboard', :to => 'dashboard#summary', :as => :dashboard
   get 'error', :to => 'application#error', :as => :error
+  get 'status', :to => 'application#status', :as => :status
 end
