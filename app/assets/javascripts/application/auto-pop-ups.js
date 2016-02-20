aeae9d2a8ae904fe22d6715cb055acdb4cab0a64
Chris Roberts <code@chrisroberts.org>
2016-02-20 07:56:26 -0800

    Clean up basic auto popup implementation to allow delay and manual dismissal.

--- app/assets/javascripts/application/auto-pop-ups.js
+++ app/assets/javascripts/application/auto-pop-ups.js
@@ -9,7 +9,13 @@ auto_popups.show_popups = function(){
   auto_popups['items'] = [];
   $.each(all_items, function(idx, item){
     if(window[item['condition']['name']](item['condition']['args'])){
-      auto_popups.display(item)
+      if(item['delay']){
+        setTimeout(function(p_item){
+          auto_popups.display(p_item);
+        }, item['delay'] * 1000, item);
+      } else {
+        auto_popups.display(item);
+      }
     } else {
       auto_popups['items'].push(item);
     }
@@ -17,17 +23,29 @@ auto_popups.show_popups = function(){
 }
 
 auto_popups.display = function(item){
-  $('#' + item['dom_id']).popover({
+  if(item['dom_id']){
+    target = '#' + item['dom_id'];
+  } else {
+    target = item['filter'];
+  }
+  $(target).popover({
     content: item['content'],
     html: true,
     placement: item['location'],
-    title: item['title']
+    title: item['title'] + '<button type="button" class="pull-right close pcloser">&times;</button><div class="clearfix"/>'
   }).popover('show');
+  $('.pcloser').click(function(){
+    pover = $(this).parents('.popover');
+    pover.popover('hide');
+    pover.popover('destroy');
+  });
   if(item['duration']){
     setTimeout(function(){
       $('#' + item['dom_id']).popover('hide');
+      $('#' + item['dom_id']).popover('destroy');
     }, item['duration'] * 1000);
   }
 }
 
 $(document).ajaxComplete(auto_popups.show_popups);
+$(document).ready(auto_popups.show_popups);
