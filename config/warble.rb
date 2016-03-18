fb04d334c6aa069ebe7923108b4692dfd3573653
Chris Roberts <code@chrisroberts.org>
2016-03-18 16:10:56 -0700

    Disable home override

--- config/warble.rb
+++ config/warble.rb
@@ -2,7 +2,7 @@ Warbler::Config.new do |config|
   config.features = %w(executable)
   config.java_libs += FileList["lib/java/*.jar"]
   config.jar_name = "fission-#{Time.now.to_i}"
-  config.override_gem_home = true
+  config.override_gem_home = false
   config.webserver = 'jetty'
   config.webxml.jruby.rack.logging = 'slf4j'
   config.includes += FileList['public/assets/manifest-*.json'].existing
