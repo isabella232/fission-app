8a4f2a2f83f6606bf213311cf23351e03169054d
Chris Roberts <code@chrisroberts.org>
2016-03-18 17:03:27 -0700

    Use consistent jar ball asset name

--- config/warble.rb
+++ config/warble.rb
@@ -1,7 +1,7 @@
 Warbler::Config.new do |config|
   config.features = %w(executable)
   config.java_libs += FileList["lib/java/*.jar"]
-  config.jar_name = "fission-#{Time.now.to_i}"
+  config.jar_name = "fission-app"
   config.override_gem_home = false
   config.webserver = 'jetty'
   config.webxml.jruby.rack.logging = 'slf4j'
